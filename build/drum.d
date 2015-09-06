b53cd920972375165c300cd9b608986ae389695c
Tolm Dyn <tolm.dyn@gmail.com>
2015-09-06 18:02:41 +0100

    Adding compiled binaries

---
+++ build/drum.d
@@ -0,0 +1,73 @@
+drum.o: /home/tolm/dev/GBA/projects/drum/source/drum.c \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_affine.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_base.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_types.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_compression.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_console.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_dma.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_input.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_interrupt.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_multiboot.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_sio.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_sound.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_sprites.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_systemcalls.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_timers.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/gba_video.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/maxmod.h \
+ /home/tolm/dev/GBA/devkitPro/libgba/include/mm_types.h \
+ /home/tolm/dev/GBA/projects/drum/build/soundbank.h \
+ /home/tolm/dev/GBA/projects/drum/build/soundbank_bin.h \
+ /home/tolm/dev/GBA/projects/drum/build/r6502_portfont_bin.h \
+ /home/tolm/dev/GBA/projects/drum/include/include.h \
+ /home/tolm/dev/GBA/projects/drum/include/palette.h \
+ /home/tolm/dev/GBA/projects/drum/include/help.h
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_affine.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_base.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_types.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_compression.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_console.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_dma.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_input.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_interrupt.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_multiboot.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_sio.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_sound.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_sprites.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_systemcalls.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_timers.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/gba_video.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/maxmod.h:
+
+/home/tolm/dev/GBA/devkitPro/libgba/include/mm_types.h:
+
+/home/tolm/dev/GBA/projects/drum/build/soundbank.h:
+
+/home/tolm/dev/GBA/projects/drum/build/soundbank_bin.h:
+
+/home/tolm/dev/GBA/projects/drum/build/r6502_portfont_bin.h:
+
+/home/tolm/dev/GBA/projects/drum/include/include.h:
+
+/home/tolm/dev/GBA/projects/drum/include/palette.h:
+
+/home/tolm/dev/GBA/projects/drum/include/help.h:
